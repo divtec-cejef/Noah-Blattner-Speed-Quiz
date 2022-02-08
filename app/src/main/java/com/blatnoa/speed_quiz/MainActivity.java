b922904d249ada740c8578fbdf64dd010faa55d7
NoahBlattner <noah.blattner@divtec.ch>
2022-02-08 14:45:24 +0100

    Added menu to welcome page

--- app/src/main/java/com/blatnoa/speed_quiz/MainActivity.java
+++ app/src/main/java/com/blatnoa/speed_quiz/MainActivity.java
@@ -1,8 +1,11 @@
 package com.blatnoa.speed_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater =getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
