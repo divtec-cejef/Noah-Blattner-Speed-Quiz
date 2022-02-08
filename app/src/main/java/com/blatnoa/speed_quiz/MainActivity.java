9f712bbadaedd84e0ba6f0ff3b10a98ac8eab7b3
NoahBlattner <noah.blattner@divtec.ch>
2022-02-08 15:35:10 +0100

    Added onClick to edit players button

--- app/src/main/java/com/blatnoa/speed_quiz/MainActivity.java
+++ app/src/main/java/com/blatnoa/speed_quiz/MainActivity.java
@@ -6,8 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button editPlayersButton;
+    private Button playButton;
+    private TextInputLayout player1InputLayout;
+    private TextInputLayout player2InputLayout;
+    private EditText player1Edit;
+    private EditText player2Edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +28,44 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
+
+        editPlayersButton = findViewById(R.id.main_button_edit_players);
+        playButton = findViewById(R.id.main_button_play);
+
+        player1InputLayout = findViewById(R.id.input_layout_player1);
+        player1InputLayout.setVisibility(View.INVISIBLE);
+        player2InputLayout = findViewById(R.id.input_layout_player2);
+        player2InputLayout.setVisibility(View.INVISIBLE);
+
+        player1Edit = findViewById(R.id.edit_first_player);
+        player2Edit = findViewById(R.id.edit_second_player);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        editPlayersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if (player1InputLayout.getVisibility() == View.INVISIBLE) {
+                   player1InputLayout.setVisibility(View.VISIBLE);
+                   player2InputLayout.setVisibility(View.VISIBLE);
+               } else {
+                   player1InputLayout.setVisibility(View.INVISIBLE);
+                   player2InputLayout.setVisibility(View.INVISIBLE);
+               }
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!player1Edit.getText().toString().isEmpty() && !player2Edit.getText().toString().isEmpty()) {
+                    //Intent intent = new Intent()
+                }
+            }
+        });
     }
 
     @Override
