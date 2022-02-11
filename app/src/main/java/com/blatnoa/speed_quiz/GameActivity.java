9f10a5212970d1e184d30dabb41aac1b84197904
NoahBlattner <noah.blattner@divtec.ch>
2022-02-11 11:48:29 +0100

    Built game activity

--- app/src/main/java/com/blatnoa/speed_quiz/GameActivity.java
+++ app/src/main/java/com/blatnoa/speed_quiz/GameActivity.java
@@ -3,11 +3,14 @@ package com.blatnoa.speed_quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
